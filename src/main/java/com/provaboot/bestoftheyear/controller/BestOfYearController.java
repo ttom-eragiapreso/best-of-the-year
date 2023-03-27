9896a474e34e2eab2956a19ba169de31742d573d
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-27 15:01:17 +0200

    finito step 2

--- src/main/java/com/provaboot/bestoftheyear/controller/BestOfYearController.java
+++ src/main/java/com/provaboot/bestoftheyear/controller/BestOfYearController.java
@@ -1,9 +1,14 @@
 package com.provaboot.bestoftheyear.controller;
 
+import com.provaboot.bestoftheyear.movie.Movie;
+import com.provaboot.bestoftheyear.song.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +18,61 @@ public class BestOfYearController {
     public String getHome(Model model){
         model.addAttribute("name", "Enrico");
         return "home";
+    }
+    @GetMapping("/songs")
+    public String getBestSongs(Model model){
+
+        List<Song> bestSongs = listBestSongs();
+        String output = "";
+        for (Song s :
+                bestSongs) {
+            output+= s.getTitle() + ", ";
+        }
+
+        String result = output.substring(0, output.length() -2 );
+
+        model.addAttribute("songs", result + ".");
+        return "songs";
+    }
+
+    @GetMapping("/movies")
+    public String getBestMovies(Model model){
+
+        List<Movie> bestMovies = listBestMovies();
+        String output = "";
+
+        for (Movie m :
+                bestMovies) {
+            output+=m.getTitle() + ", ";
+        }
+
+        output = output.substring(0, output.length() -2);
+
+        model.addAttribute("movies", output + ".");
+
+        return "movies";
+    }
+
+    private List<Song> listBestSongs(){
+        List<Song> bestSongs = new ArrayList<>();
+
+        bestSongs.add(new Song(1, "Scar Tissue"));
+        bestSongs.add(new Song(2, "Californication"));
+        bestSongs.add(new Song(3, "Dani California"));
+        bestSongs.add(new Song(4, "Can't Stop"));
+
+
+        return bestSongs;
+    }
+    private List<Movie> listBestMovies(){
+        List<Movie> bestMovies = new ArrayList<>();
+
+        bestMovies.add(new Movie(1, "Toy Story"));
+        bestMovies.add(new Movie(2, "Avatar"));
+        bestMovies.add(new Movie(3, "The Gladiator"));
+        bestMovies.add(new Movie(4, "Lord of the Rings"));
+
+
+        return bestMovies;
     }
 }
