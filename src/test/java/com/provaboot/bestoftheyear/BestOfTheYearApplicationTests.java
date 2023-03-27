f0c27ceaba76fef6cbc72491047646beb351d97d
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-27 14:35:15 +0200

    finito step 1

---
+++ src/test/java/com/provaboot/bestoftheyear/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provaboot.bestoftheyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
